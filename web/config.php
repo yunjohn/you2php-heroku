00ead70cf3175eb0a5b9ec4fc13a1b0af3042470
yunjohn <38034282+yunjohn@users.noreply.github.com>
2018-04-04 08:53:00 +0800

    Update config.php

--- web/config.php
+++ web/config.php
@@ -1,9 +1,9 @@
 <?php
 define('ROOT_PART', Root_part());
 define('APIKEY', 'AIzaSyCvL53uhVDVM2tSxnvNhiJn5jnId7qw2Og');//这里改成您的api key
-define('GJ_CODE', 'KR');//国家代码
-define('SITE_NAME', 'Youtube');//网站名字
-define('TITLENAME', 'Youtube');//网站名字
+define('GJ_CODE', 'TW');//国家代码
+define('SITE_NAME', 'Yuntube');//网站名字
+define('TITLENAME', 'Yuntube');//网站名字
 define('EN2DEKEY', 'AnQUP1XDEF435e67CtzDSh4');//一个随机字符串，加解密KEY
-define('EMAIL', '123@123.com');//邮箱
+define('EMAIL', 'xuyun5566@gmail.com');//邮箱
 ?>
